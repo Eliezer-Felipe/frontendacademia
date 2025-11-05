6f11f3502a80dd17b349f40a6146ee1a4b33678e
Eliezer Felipe <eliezerflsilva@gmail.com>
2025-11-05 18:52:12 -0300

    alterando url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configurações da aplicação
 const CONFIG = {
-    API_BASE_URL: 'backendacademia-production.up.railway.app/api',
+    API_BASE_URL: 'backendacademia-production-6ae4.up.railway.app/api',
     
     // Endpoints da API
     ENDPOINTS: {
@@ -21,4 +21,4 @@ const CONFIG = {
 };
 
 // Exportar configurações para uso global
-window.CONFIG = CONFIG;
+window.CONFIG = CONFIG
